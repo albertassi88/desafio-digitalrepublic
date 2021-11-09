f52029fbe955e4e3f3d496abe09dd244f8d3ece6
albertassi88 <albertassi88@gmail.com>
2021-11-09 15:28:13 -0300

    criacao da pagina Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GlobalStyle from "../Home/style";
+
+export default function Home() {
+    return (
+        <>
+            <GlobalStyle />
+        </>
+    );
+}
