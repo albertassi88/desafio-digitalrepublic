c347abae2c29cec45caf1cd10105b5e87a2a770a
albertassi88 <albertassi88@gmail.com>
2021-11-13 20:55:56 -0300

    criacao do teste no componente WallMeasure

---
+++ src/components/WallMeasure/index.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import WallMeasure from ".";
+
+describe("test the WallMeasure component", () => {     
+    it("check if the phrase PAREDE 1 appears on the screen", () => {
+        render(<WallMeasure />)
+        expect(screen.getByText("PAREDE 1")).toBeInTheDocument();
+    });      
+});
+  
